3930e13203fa3f17033a64d0dc87a524f87ba059
Harsh Thaker <hst.sidhpur@gmail.com>
2018-04-20 15:51:25 +0200

    Create one_hot.py

---
+++ one_hot.py
@@ -0,0 +1,12 @@
+
+
+
+def dense_to_one_hot(labels_dense, num_classes):
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+labels = dense_to_one_hot(labels_flat, labels_count)
+labels = labels.astype(np.uint8)
